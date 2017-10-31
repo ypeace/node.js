8399354b91f935d789db649f3eca86fa2129b677
heping <1075446256@qq.com>
2017-10-31 14:36:39 +0800

    2017/10/31

---
+++ node/1031/5.js
@@ -0,0 +1,10 @@
+var m5 = require('./hello');//返回值就是被加载模块中的Module.exports
+/*
+modelie:保顿提供和当前模块有关的一些信息
+
+在这个module对象里，有一个子对象：exports 对象
+我们可以通过这个对象吧一个模块中的局部变量对外进行提供访问
+* */
+//console.log( a );
+//console.log(module)
+console.log(m5);
---
+++ node/1031/hello.js
@@ -0,0 +1,17 @@
+/*
+如果我们想一个模块可以访问另一个模块中的定义变量 ，可以把变量作为
+1.global对象的属性(不推荐)
+2.使用模块对象module
+3.再模块作用域内还有一个内置的模块对象，exports 他其实就是module.exports
+* */
+//global.a = 100
+// console.log(module)
+var a = 100;
+//module.exports.a = a;
+
+exports.a = a;
+
+console.log(module.exports===exports);
+
+module.exports = [123];//如果这样做的话 exports和module.exports的指向关系就已经断开了
+//尽可能在module.exports和exports上添加属性而不是赋值
